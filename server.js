f96fc0caec026a7e61d777ba5903a9c4ed816fb1
Ajay R <rajay2658@gmail.com>
2019-12-31 01:02:48 +0530

    restructuring server.js

--- server.js
+++ server.js
@@ -38,29 +38,24 @@ app.use(
   })
 );
 app.post("/indexdata", async (req, res) => {
-  let { wordMap, paraList } = await indexDoc(req.body.data);
+  const data = data.replace(/ \n/g, "\n");
+  const paraList = data.split("\n\n");
+  const wordList = paraList.map(function(para) {
+    return Array.from(new Set(para.split(/\.|,|\s/)));
+  });
+  var wordMap = {};
+  for (var i = 0; i < wordList.length; i++) {
+    for (var j = 0; j < wordList[i].length; j++) {
+      var word = wordList[i][j].toLowerCase();
+      if (typeof wordMap[word] === "undefined") wordMap[word] = [];
+      if (wordMap[word].length <= 10) wordMap[word].push(i);
+    }
+  }
   req.session.wordIndex = wordMap;
   req.session.paraList = paraList;
   res.send("indexed");
 });
-function indexDoc(data) {
-  return new Promise(function(resolve, reject) {
-    data = data.replace(/ \n/g, "\n");
-    const paraList = data.split("\n\n");
-    const wordList = paraList.map(function(para) {
-      return Array.from(new Set(para.split(/\.|,|\s/)));
-    });
-    var wordMap = {};
-    for (var i = 0; i < wordList.length; i++) {
-      for (var j = 0; j < wordList[i].length; j++) {
-        var word = wordList[i][j].toLowerCase();
-        if (typeof wordMap[word] === "undefined") wordMap[word] = [];
-        if (wordMap[word].length <= 10) wordMap[word].push(i);
-      }
-    }
-    resolve({ wordMap, paraList });
-  });
-}
+
 app.get("/cleardata", (req, res) => {
   req.session.wordIndex = {};
   res.send("deleted");
@@ -85,21 +80,8 @@ app.get("/search", (req, res) => {
     res.send(matches);
   }
 });
-app.post("/uploadfile", (req, res) => {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
-    var extension = files.data.type;
-    if (extension.substring(extension.length - 3) == "pdf") {
-      fs.readFile(files.data.path, function(err, doc) {
-        pdf(doc).then(async function(data) {
-          let { wordMap, paraList } = await indexDoc(data.text);
-          req.session.wordIndex = wordMap;
-          req.session.paraList = paraList;
-          res.send("indexed");
-        });
-      });
-    } else {
-      res.send("error");
-    }
+function indexDoc(data) {
+  return new Promise(function(resolve, reject) {
+    resolve({ wordMap, paraList });
   });
-});
+}
