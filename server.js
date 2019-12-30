3454a630fd85ac3b2c638c1a32f7336ac011d202
Ajay R <rajay2658@gmail.com>
2019-12-31 01:59:55 +0530

    changed express.static dir url

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cors = require("cors");
 var util = require("util");
 fs = require("fs");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname + "/client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
   });
